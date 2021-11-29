45a7bc521b67f05a72c78412e865b07c0d90367a
Muhammad Waleed <77747037+blurryface92@users.noreply.github.com>
2021-11-29 13:52:08 +0500

    Add files via upload

---
+++ newsapp/app.py
@@ -0,0 +1,47 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager, login_manager
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def createApp():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "idkifitismeornot111155"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+    
+    db.init_app(app)
+    
+    from views import views
+    from auth import auth
+    
+    app.register_blueprint(views,url_prefix="/")
+    app.register_blueprint(auth,url_prefix="/")
+    
+    from models import User,Post,Comments,Likes
+    
+    create_database(app)
+    
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
+    return app
+
+def create_database(app):
+    if not path.exists("website/"+DB_NAME):
+        db.create_all(app=app)
+        print("DATABASE CREATED OWO")
+
+
+if __name__ == "__main__":
+    app = createApp()
+    app.run(debug=False)
---
+++ newsapp/models.py
@@ -0,0 +1,42 @@
+from enum import unique
+from sqlalchemy.orm import backref
+
+from sqlalchemy.sql.expression import true
+from app import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(150),unique=True)
+    email = db.Column(db.String(150),unique=True)
+    password = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True),default = func.now())
+    posts = db.relationship("Post",backref='user',passive_deletes=True)
+    comments = db.relationship("Comments",backref='user',passive_deletes=True)
+    likes = db.relationship("Likes",backref='user',passive_deletes=True)
+    
+    
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(150),nullable=True)
+    post = db.Column(db.Text,nullable=True)
+    date_created = db.Column(db.DateTime(timezone=True),default = func.now())
+    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete="CASCADE"),nullable=False)
+    comments = db.relationship("Comments",backref='post',passive_deletes=True)
+    likes = db.relationship("Likes",backref='post',passive_deletes=True)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    text = db.Column(db.String(150),nullable=True)
+    date_created = db.Column(db.DateTime(timezone=True),default = func.now())
+    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete="CASCADE"),nullable=False)
+    post_id = db.Column(db.Integer,db.ForeignKey('post.id',ondelete="CASCADE"),nullable=False)
+
+
+class Likes(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    date_created = db.Column(db.DateTime(timezone=True),default = func.now())
+    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete="CASCADE"),nullable=False)
+    post_id = db.Column(db.Integer,db.ForeignKey('post.id',ondelete="CASCADE"),nullable=False)
